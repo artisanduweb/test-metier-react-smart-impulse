336f5367757d7193599a60fabc3c5364e6cb20dd
lartisanduweb <lartisanduweb@gmail.com>
2024-06-16 14:49:46 +0200

    SIT #6: configure path alias

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
+import { Button  } from "@/components/ui/button";
 
 class App extends Component {
   constructor(props) {
