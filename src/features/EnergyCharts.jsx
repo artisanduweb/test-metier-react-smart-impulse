d89f2ec27adf27d0abd4039eae3d9f01746f6ce6
lartisanduweb <lartisanduweb@gmail.com>
2024-06-18 08:02:22 +0200

    SIT #13: Add date range picker to energychart

--- src/features/EnergyCharts.jsx
+++ src/features/EnergyCharts.jsx
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, {useCallback, useEffect, useState } from 'react';
 import ReactEcharts from 'echarts-for-react';
+import { DatePickerWithRange } from '@/components/custom/date-range-picker';
+import { addDays,  } from "date-fns"
 
 export const EnergyChart = ({data}) => {
-  const dateFormatter = new Intl.DateTimeFormat('fr-FR', {
-    weekday: 'long',
-    day: '2-digit',
-    month: '2-digit',
-    year: 'numeric',
-  });
 
-  const getOptions = () => {
+  const [date, setDate] = useState({
+    from: new Date(2024, 5, 20),
+    to: addDays(new Date(2024, 5, 20), 20),
+  })
+
+  // Hook to go to a specific date where the data is available
+  useEffect(() => {
+    if (data.length) {
+      const maxDate = new Date(Math.max(...data.map(series => series.data[series.data.length - 1][0])))
+      const minDate = addDays(maxDate, -15)
+      setDate({ from: minDate, to: maxDate });
+    }
+  }, [data])
+
+  const filterDataByDate = (data, fromDate, toDate) => {
+    return data.map(series => ({
+      ...series,
+      data: series.data.filter(point => {
+        const date = new Date(point[0]);
+        return date >= fromDate && date <= toDate;
+      }),
+    }));
+  };
+
+  const getOptions = useCallback(() => {
+    const dateFormatter = new Intl.DateTimeFormat('fr-FR', {
+      weekday: 'long',
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+    });
+
+    const filteredData = date && date.from && date.to ? filterDataByDate(data, date.from, date.to) : data
+
     return {
       title: {
         text: 'Energy Consumption',
@@ -44,7 +73,7 @@ export const EnergyChart = ({data}) => {
         },
       },
     
-      series:  data.map((d) => ({
+      series:  filteredData.map((d) => ({
         name: d.label,
         type: 'bar',
         barWidth: '100%',
@@ -53,11 +82,22 @@ export const EnergyChart = ({data}) => {
         color: d.color,
       })),
     };
-  }
+  }, [data, date]);
 
-  return <ReactEcharts
-          option={getOptions()}
-          showLoading={data.length === 0} 
-          loadingOption={{ text: 'Loading...' }}
-        />;
+
+
+  return <>
+    <ReactEcharts
+      option={getOptions()}
+      showLoading={data.length === 0} 
+      loadingOption={{ text: 'Loading...' }}
+    />
+    <div className="flex flex-col gap-3 py-2">
+      <span className="text-sm font-semibold opacity-80">
+        Pick the period
+      </span>
+      <DatePickerWithRange date={date} setDate={setDate}/>
+    </div>
+    {/* TODO: Euro */}
+  </>
 }
